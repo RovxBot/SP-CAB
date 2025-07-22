879f0eb8b13b0569887ab4d5751fca24b45d36ac
Rovx <sam@cooked.beer>
2025-07-22 13:29:05 +0930

    Changed up certificate auth handeling for now Added some more readme infor on generating certs

--- deployment/scripts/ingest-message-centre.ps1
+++ deployment/scripts/ingest-message-centre.ps1
@@ -1,11 +1,14 @@
 #certificate built from SP_CERT_PEM and stored temporarily
-$certPath = "$env:TEMP\sp-cert.pem"
+$certPath = "./deployment/scripts/CABDeployment.pfx"
+
 # Connect to graph API
 $token = Get-GraphToken -ClientID $env:INGESTION_CLIENT_ID -TenantId $env:DEPLOYMENT_TENANT_ID -ClientSecret $env:INGESTION_CLIENT_SECRET
+
 # Collect updates from the message centre
 $updates = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/admin/serviceAnnouncement/messages" -Headers @{Authorization = "Bearer $token"}
+
 # Connect to SharePoint with PnP PowerShell
-Connect-PnPOnline -Url $env:SHAREPOINT_SITE_URL -ClientId $env:INGESTION_CLIENT_ID -Tenant $env:DEPLOYMENT_TENANT_ID -CertificatePath $certPath -CertificatePassword ""
+Connect-PnPOnline -Url $env:DEPLOYMENT_SITE_URL -ClientId $env:INGESTION_CLIENT_ID -Tenant $env:DEPLOYMENT_TENANT_ID -CertificatePath $certPath -CertificatePassword ""
 
 # Push updates to the Microsoft Changes List
 foreach ($update in $updates.value) {
